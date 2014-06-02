bb6a610713ace755ea10c84e4ae97629f0d8680c
inferrna <inferrna@gmail.com>
2014-06-02 18:29:57 +0900

    +some asm.js works

--- Client/WebQuake/GL.js
+++ Client/WebQuake/GL.js
@@ -1,8 +1,77 @@
 GL = {};
-
+var BUFFER_SIZE = 4194304;
 GL.textures = [];
 GL.currenttextures = [];
 GL.programs = [];
+function AsmFuncs(stdlib, env, heap) {
+    "use asm";
+    var sin = stdlib.Math.sin;
+    var cos = stdlib.Math.cos;
+    var floor = stdlib.Math.floor;
+    var res32f = new stdlib.Float32Array(heap);
+    var res8i = new stdlib.Uint8Array(heap);
+    var inp8i = new stdlib.Uint8Array(heap);
+    function rotation_matrix(pitch, yaw, roll){
+        pitch = +pitch;
+        yaw = +yaw;
+        roll = +roll;
+        var sp = 0.0;
+        var cp = 0.0;
+        var sy = 0.0;
+        var cy = 0.0;
+        var sr = 0.0;
+        var cr = 0.0;
+        var PI = 3.141592653589793;
+        pitch = -1.0*pitch*PI/180.0;
+        yaw   = yaw*PI/180.0;
+        roll  = roll*PI/180.0;
+        sp = +sin(pitch);
+        cp = +cos(pitch);
+        sy = +sin(yaw);
+        cy = +cos(yaw);
+        sr = +sin(roll);
+        cr = +cos(roll);
+        res32f[0] =  cy * cp;
+        res32f[1] =  sy * cp;
+        res32f[2] =  -sp;
+        res32f[3] =  -sy * cr + cy * sp * sr;
+        res32f[4] =  cy * cr + sy * sp * sr;
+        res32f[5] =   cp * sr;
+        res32f[6] =  -sy * -sr + cy * sp * cr;
+        res32f[7] =  cy * -sr + sy * sp * cr;
+        res32f[8] =  cp * cr;
+    }
+    function scale_texture(inwidth, inheight, outwidth, outheight){
+        inwidth = inwidth|0;
+        inheight = inheight|0;
+        outwidth = outwidth|0;
+        outheight = outheight|0;
+        var xstep = 0.0;
+        var ystep = 0.0;
+        var mul = 0.0;
+        var dest = 0;
+        var i = 0;
+        var j = 0;
+        var src = 0;
+        var bs = 0;
+        i = i|0; j = j|0; dest = dest|0; src = src|0; bs = 0|0;
+        bs = 4194304;
+        xstep = (+~~inwidth)/(+~~outwidth);
+        ystep = (+~~inheight)/(+~~outheight);
+        for (i = 1; (i | 0) < (outheight | 0); i = (i | 0) + 1 | 0){
+            mul = +floor((+~~i)*ystep);
+            src = (~~mul)|0 * inwidth;
+            for (j = 1; (j | 0) < (outwidth | 0); j = (j | 0) + 1 | 0){
+                res8i[dest+j|0] = 255;//inp8i[(bs>>1) + src|0+(~~+floor((+~~j)*xstep))];
+                dest = dest+outwidth|0;
+            }
+        }
+    }
+    return {rotation_matrix: rotation_matrix, scale_texture: scale_texture}; //asm_funcs.scale_texture
+}
+
+var buffer = new ArrayBuffer(BUFFER_SIZE);
+var asm_funcs = AsmFuncs(window, 0, buffer);
 
 GL.Bind = function(target, texnum)
 {
@@ -81,7 +150,15 @@ GL.Set2D = function()
 
 GL.ResampleTexture = function(data, inwidth, inheight, outwidth, outheight)
 {
+    var heapin = new Uint8Array(buffer, BUFFER_SIZE >> 1, inwidth*inheight);
+    var heapout = new Uint8Array(buffer, 0, outwidth*outheight);
+    heapin.set(data);
+    asm_funcs.scale_texture(inwidth, inheight, outwidth, outheight); 
 	var outdata = new ArrayBuffer(outwidth * outheight);
+	var out = new Uint8Array(outdata);
+    out.set(heapout);
+    return out;
+	/*var outdata = new ArrayBuffer(outwidth * outheight);
 	var out = new Uint8Array(outdata);
 	var xstep = inwidth / outwidth, ystep = inheight / outheight;
 	var src, dest = 0, y;
@@ -93,7 +170,7 @@ GL.ResampleTexture = function(data, inwidth, inheight, outwidth, outheight)
 			out[dest + j] = data[src + Math.floor(j * xstep)];
 		dest += outwidth;
 	}
-	return out;
+	return out;*/
 };
 
 GL.Upload = function(data, width, height)
@@ -320,9 +397,11 @@ GL.UnbindProgram = function()
 
 GL.identity = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0];
 
+
+
 GL.RotationMatrix = function(pitch, yaw, roll)
 {
-	pitch *= Math.PI / -180.0;
+	/*pitch *= Math.PI / -180.0;
 	yaw *= Math.PI / 180.0;
 	roll *= Math.PI / 180.0;
 	var sp = Math.sin(pitch);
@@ -335,7 +414,10 @@ GL.RotationMatrix = function(pitch, yaw, roll)
 		cy * cp,					sy * cp,					-sp,
 		-sy * cr + cy * sp * sr,	cy * cr + sy * sp * sr,		cp * sr,
 		-sy * -sr + cy * sp * cr,	cy * -sr + sy * sp * cr,	cp * cr
-	];
+	];*/
+    asm_funcs.rotation_matrix(pitch, yaw, roll);
+    return Array.prototype.slice.call(new Float32Array(buffer, 0, 9));
+    //new Float32Array(buffer, 0, 9);
 };
 
 GL.Init = function()
---
+++ Client/par-0.03.01/buildjs.sh
@@ -0,0 +1,3 @@
+#emcc -Dhtml -O1 -g2 -s ASSERTIONS=2 -s NO_EXIT_RUNTIME=1 --llvm-lto 3 --llvm-opts "['-vectorize-loops', '-enable-unsafe-fp-math', '-enable-fp-mad']" par.c -o par.js
+emcc -Dhtml -O3 -s NO_EXIT_RUNTIME=1 --llvm-lto 3 --llvm-opts "['-vectorize-loops', '-enable-unsafe-fp-math', '-enable-fp-mad']" par.c -o par.js
+
