f170dbaa1b4649a59271f5af12cc8a3059abbbf2
inferrna <inferrna@gmail.com>
2014-06-03 18:39:14 +0900

    +Move Vec to typed arrays

--- Client/WebQuake/Vec.js
+++ Client/WebQuake/Vec.js
@@ -36,6 +36,7 @@ Vec.Perpendicular = function(v)
 
 Vec.RotatePointAroundVector = function(dir, point, degrees)
 {
+    //console.log("Call to Vec.RotatePointAroundVector");
 	var r = Vec.Perpendicular(dir);
 	var up = Vec.CrossProduct(r, dir);
 	var m = new Float32Array([
@@ -198,21 +199,15 @@ Vec.Normalize = function(v)
 
 Vec.ConcatRotations = function(m1, m2)
 {
-	return [
-		[
-			m1[0][0] * m2[0][0] + m1[0][1] * m2[1][0] + m1[0][2] * m2[2][0],
-			m1[0][0] * m2[0][1] + m1[0][1] * m2[1][1] + m1[0][2] * m2[2][1],
-			m1[0][0] * m2[0][2] + m1[0][1] * m2[1][2] + m1[0][2] * m2[2][2]
-		],
-		[
-			m1[1][0] * m2[0][0] + m1[1][1] * m2[1][0] + m1[1][2] * m2[2][0],
-			m1[1][0] * m2[0][1] + m1[1][1] * m2[1][1] + m1[1][2] * m2[2][1],
-			m1[1][0] * m2[0][2] + m1[1][1] * m2[1][2] + m1[1][2] * m2[2][2]
-		],
-		[
-			m1[2][0] * m2[0][0] + m1[2][1] * m2[1][0] + m1[2][2] * m2[2][0],
-			m1[2][0] * m2[0][1] + m1[2][1] * m2[1][1] + m1[2][2] * m2[2][1],
-			m1[2][0] * m2[0][2] + m1[2][1] * m2[1][2] + m1[2][2] * m2[2][2]
-		]
-	];
+	return new Float32Array([
+			m1[0] * m2[0] + m1[1] * m2[3] + m1[2] * m2[6],
+			m1[0] * m2[1] + m1[1] * m2[4] + m1[2] * m2[7],
+			m1[0] * m2[2] + m1[1] * m2[5] + m1[2] * m2[8],
+			m1[3] * m2[0] + m1[4] * m2[3] + m1[5] * m2[6],
+			m1[3] * m2[1] + m1[4] * m2[4] + m1[5] * m2[7],
+			m1[3] * m2[2] + m1[4] * m2[5] + m1[5] * m2[8],
+			m1[6] * m2[0] + m1[7] * m2[3] + m1[8] * m2[6],
+			m1[6] * m2[1] + m1[7] * m2[4] + m1[8] * m2[7],
+			m1[6] * m2[2] + m1[7] * m2[5] + m1[8] * m2[8]
+	]);
 };
