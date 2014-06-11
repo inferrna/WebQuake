c118e695b0db90ded405b4689c8de181645d1446
inferrna <inferrna@gmail.com>
2014-06-11 18:30:31 +0900

    +tries in par

--- Client/par-0.03.01/buildjs.sh
+++ Client/par-0.03.01/buildjs.sh
@@ -1,3 +1,4 @@
 #emcc -Dhtml -O1 -g2 -s ASSERTIONS=2 -s NO_EXIT_RUNTIME=1 --llvm-lto 3 --llvm-opts "['-vectorize-loops', '-enable-unsafe-fp-math', '-enable-fp-mad']" par.c -o par.js
-emcc -Dhtml -O3 -s NO_EXIT_RUNTIME=1 --llvm-lto 3 --llvm-opts "['-vectorize-loops', '-enable-unsafe-fp-math', '-enable-fp-mad']" par.c -o par.js
+#emcc -Dhtml -O3 -s NO_EXIT_RUNTIME=1 --llvm-lto 3 --llvm-opts "['-vectorize-loops', '-enable-unsafe-fp-math', '-enable-fp-mad']" /home/inferno/.dev/emscripten/system/lib/dlmalloc.c par.c -o par.js
+emcc -Dhtml -O3 -s NO_EXIT_RUNTIME=1 --llvm-lto 3 --llvm-opts "['-vectorize-loops', '-enable-unsafe-fp-math', '-enable-fp-mad']" dlmalloc.c -o dlmalloc.js
 
