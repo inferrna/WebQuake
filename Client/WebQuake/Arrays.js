32845fc2035ee3f505709381d49b3d4946d74cdf
inferrna <inferrna@gmail.com>
2014-06-24 17:02:03 +0900

    +malloc better

--- Client/WebQuake/Arrays.js
+++ Client/WebQuake/Arrays.js
@@ -1,4 +1,4 @@
-mUint16Array = function(param){
+mUint16Array = function(param, off, ln){
     const $M = memory;
     const $malloc = $M.malloc, $U2 = $M.U2;
     if(param instanceof Array || param instanceof Uint8Array || param instanceof Uint16Array ){
@@ -13,12 +13,13 @@ mUint16Array = function(param){
         this._addr =  $malloc((2 * param|0)>>>0) >> 1;
         var res = $U2.subarray(this._addr, this._addr+this._len);
     } else if(param instanceof ArrayBuffer){
-        var res = new Uint16Array(param);
+        if(off || ln) var res = new Uint16Array(param, off, ln);
+        else          var res = new Uint16Array(param);
     }
     //console.log(this._addr);
     return res;
 };
-mUint8Array = function(param){
+mUint8Array = function(param, off, ln){
     const $M = memory;
     const $malloc = $M.malloc, $U1 = $M.U1;
     if(param instanceof Array || param instanceof Uint8Array){
@@ -33,12 +34,13 @@ mUint8Array = function(param){
         this._addr =  $malloc((param|0)>>>0);
         var res = $U1.subarray(this._addr, this._addr+this._len);
     } else if(param instanceof ArrayBuffer){
-        var res = new Uint8Array(param);
+        if(off || ln) var res = new Uint8Array(param, off, ln);
+        else          var res = new Uint8Array(param);
     }
     //console.log(this._addr);
     return res;
 };
-mFloat32Array = function(param){
+mFloat32Array = function(param, off, ln){
     const $M = memory;
     const $malloc = $M.malloc, $F4 = $M.F4;
     if(param instanceof Array || param instanceof Float32Array || param instanceof Float64Array){
@@ -53,7 +55,8 @@ mFloat32Array = function(param){
         this._addr =  $malloc((4 * param|0)>>>0) >> 2;
         var res = $F4.subarray(this._addr, this._addr+this._len);
     } else if(param instanceof ArrayBuffer){
-        var res = new Float32Array(param);
+        if(off || ln) var res = new Float32Array(param, off, ln);
+        else          var res = new Float32Array(param);
     }
     //console.log(this._addr);
     return res;
